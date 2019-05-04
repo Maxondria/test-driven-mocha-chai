966ae29983477869de88ecb58e0bd7abc9460f96
Maxondria <maxtayebwa@gmail.com>
2019-05-04 21:47:49 +0300

    added the 'nock' module to mock the server and added functionality to do so

--- test/tools-spec.js
+++ test/tools-spec.js
@@ -1,4 +1,5 @@
 const expect = require('chai').expect;
+const nock = require('nock');
 const should = require('chai').should;
 const assert = require('chai').assert;
 const tools = require('../lib/tools');
@@ -16,9 +17,17 @@ describe('tools', () => {
 	});
 
 	describe('LoadWiki()', () => {
+		//mock web server
+		before(() => {
+			nock('https://en.wikipedia.org')
+				.get('/wiki/Abraham_Lincoln')
+				.reply(200, 'Mock Abraham Lincoln Page');
+        });
+        
 		it('should load Abraham Lincoln page', done => {
 			tools.LoadWiki({ first: 'Abraham', last: 'Lincoln' }, html => {
-				expect(html).to.be.ok; //check if defined
+                expect(html).to.be.ok; //check if defined
+                expect(html).to.be.equal('Mock Abraham Lincoln Page');
 				done();
 			});
 		});
