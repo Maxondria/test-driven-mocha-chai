24e1e8e8e17b9177c5adab2111dc7167ca1a0bad
Maxondria <maxtayebwa@gmail.com>
2019-05-04 20:34:32 +0300

    added in async tests

--- test/tools-spec.js
+++ test/tools-spec.js
@@ -1,18 +1,26 @@
-
 const expect = require('chai').expect;
 const should = require('chai').should;
 const assert = require('chai').assert;
 const tools = require('../lib/tools');
 
 describe('tools', () => {
-    describe("printName()", () => {
-        it('It should print the first name first', () => {
-           const results = tools.printName({
-               first: "Max",
-               second: "Tayebwa"
-           });
-    
-           expect(results).to.equal("Tayebwa, Max");
-        });
-    });
+	describe('printName()', () => {
+		it('It should print the first name first', () => {
+			const results = tools.printName({
+				first: 'Max',
+				second: 'Tayebwa',
+			});
+
+			expect(results).to.equal('Tayebwa, Max');
+		});
+	});
+
+	describe('LoadWiki()', () => {
+		it('should load Abraham Lincoln page', done => {
+			tools.LoadWiki({ first: 'Abraham', last: 'Lincoln' }, html => {
+				expect(html).to.be.ok; //check if defined
+				done();
+			});
+		});
+	});
 });
